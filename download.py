da03de938a457592c3f4b29a184c976e8a9b75fd
Sean Behan <inbox@seanbehan.com>
2022-12-11 17:43:34 -0500

    Add matplotlib for mean prices

--- download.py
+++ download.py
@@ -1,31 +1,29 @@
-
 import pandas as pd
 from lxml.html import fromstring
 from requests import get
+import matplotlib.pyplot as plt
+from datetime import datetime
 
+now = datetime.now()
 url = 'https://www.apple.com/shop/refurbished/mac'
 
 doc = fromstring(get(url).text)
+historical_headers = ['date', 'time', 'url', 'text', 'price', 'ram', 'hd']
 
-
-
-'''
-given a url try to download the detail page
-its an unordered list so we are guessing the location for similar products
-'''
 def download_detail_page(row):
-    try:
-        url = row['url']
-        doc2 = fromstring(get(url).text)
-        price = [el for el in doc2.xpath('//div[@class="rf-pdp-currentprice"]/text()')][0]
-        ram,hd = [el.strip() for el in doc2.xpath('//div[contains(@class, "rc-pdsection-mainpanel")]/div[@class="para-list"]/p/text()')[3:5]]
-        row['price'] = price
-        row['ram'] = ram
-        row['hd'] = hd
+    row['date'] = now.strftime('%Y-%m-%d')
+    row['time'] = now.strftime('%I:%M %p')
+    url = row['url'] = 'https://apple.com' + row['path']
 
-        return row
-    except:
-        return None
+    doc_ = fromstring(get(url).text)
+    price = [el for el in doc_.xpath('//div[@class="rf-pdp-currentprice"]/text()')][0]
+    ram,hd = [el.strip() for el in doc_.xpath('//div[contains(@class, "rc-pdsection-mainpanel")]/div[@class="para-list"]/p/text()')[3:5]]
+
+    row['price'] = price
+    row['ram'] = ram
+    row['hd'] = hd
+
+    return row
 
 '''
 steps:
@@ -36,30 +34,42 @@ steps:
 '''
 df = (
     pd.DataFrame([
-      {'text': a.text, 'url': a.get('href')} 
+      {'text': a.text, 'path': a.get('href')} 
       for a in doc.xpath('//a') if 'shop/product' in a.get('href')
     ])
     .query("text.str.contains('intel', case=False)")
     .query("text.str.contains('book', case=False)")
     .query("text.str.contains('16')")
-    .assign(
-        url = lambda x: 'https://apple.com' + x.url.astype(str),
-        # details = lambda x: x.url.apply(download_detail_page),
-    )
     .apply(download_detail_page, axis=1)
 )
 
 df.to_csv('products.csv', index=False)
+df[historical_headers].to_csv('historical.csv', index=False, header=False, mode='a')
 
+historical = (
+    pd.read_csv('historical.csv', names=historical_headers)
+    .assign(
+        price = lambda x: pd.to_numeric(
+            x.price.str.replace(r'[$\,]', '', regex=True),
+            errors='coerce'
+        )
+    )
+)
 
-'''
-make markdown readme
-'''
+means = (historical.groupby('date')['price'].mean().reset_index(name='price'))
+
+fig,ax = plt.subplots(figsize=(12,8))
+ax.set(title='Mean price over time', ylabel='Prices', xlabel='Date')
+ax.bar(means.date.values, means.price.values)
+fig.savefig('prices.jpg')
+
 
 text = '''
 # 16 Inch Refurbished Macbook Pros
 
 This page updates once an hour with the latest refurbished products from Apple.com. 
+
+![Prices over time](prices.jpg?raw=true "Prices over time")
 
 '''
 for row in df.to_dict('records'):
@@ -73,5 +83,3 @@ for row in df.to_dict('records'):
 
 with open('README.md', 'w') as f:
     f.write(text)
-    
-# finito!    
