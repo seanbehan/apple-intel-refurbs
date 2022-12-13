651831e1c2c430ce979c0496ec4949200e64e92f
Sean Behan <inbox@seanbehan.com>
2022-12-13 16:10:11 -0500

    Update download.py

--- download.py
+++ download.py
@@ -50,8 +50,7 @@ df_ = (
             errors='coerce'
         )
     )
-    .query("text.str.contains('book', case=False)")
-    .query("text.str.contains('air', case=False)")
+    .query("text.str.contains('intel', case=False)")
 )
 
 
